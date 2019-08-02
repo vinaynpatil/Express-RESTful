dbb6245689df12d6798b30ac66a9df35c88e740d
vinaynpatil <reachvinaynp@gmail.com>
2019-08-02 00:03:22 -0500

    Adding books endpoint and models

---
+++ models/bookModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const bookModel = new Schema(
+  {
+    title: { type: String },
+    genre: { type: String },
+    author: { type: String },
+    read: { type: Boolean, default: false }
+  }
+);
+
+module.exports = mongoose.model('Book', bookModel);
