dabb07910bcf977a87627ef0530c0c48807233e2
vinaynpatil <reachvinaynp@gmail.com>
2019-08-03 17:07:51 -0500

    Adding middleware and HTTP verbs(post, put, patch and delete)

--- app.js
+++ app.js
@@ -1,36 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 
 const app = express();
 const db = mongoose.connect('mongodb://localhost/bookAPI');
-const bookRouter = express.Router();
+
 // Gets the port from the nodemon configuration from the package.json file
 const port = process.env.PORT || 3000;
+// This is a mongoose model
 const Book = require('./models/bookModel');
+const bookRouter = require('./routes/bookRouter')(Book);
 
-bookRouter.route('/books')
-  .get((req, res) => {
-    const query = {};
-    if (req.query.genre) {
-      query.genre = req.query.genre;
-    }
-    Book.find(query, (err, books) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(books);
-    });
-  });
-
-bookRouter.route('/books/:bookId')
-  .get((req, res) => {
-    Book.findById(req.params.bookId, (err, book) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(book);
-    });
-  });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // Wiring up the api
 app.use('/api', bookRouter);
